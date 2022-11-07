b9dfada98c72419ff03644cd41a816360698ded5
sasha52205 <55660001+sasha52205@users.noreply.github.com>
2022-11-07 09:11:25 +0300

    Update form.js

--- form.js
+++ form.js
@@ -14,10 +14,10 @@ tg.MainButton.enable()
 document.addEventListener("DOMContentLoaded", function() { // событие загрузки страницы
 
     // выбираем на странице все элементы типа textarea и input
-    document.querySelectorAll('textarea, input').forEach(function(e) {
+    document.querySelectorAll('select, input').forEach(function(e) {
         // если данные значения уже записаны в sessionStorage, то вставляем их в поля формы
         // путём этого мы как раз берём данные из памяти браузера, если страница была случайно перезагружена
-        if(e.value === '') e.value = window.localStorage.getItem(e.name, e.value);
+        if(e.value == "") e.value = window.localStorage.getItem(e.name, e.value);
         // на событие ввода данных (включая вставку с помощью мыши) вешаем обработчик
         e.addEventListener('input', function() {
             // и записываем в sessionStorage данные, в качестве имени используя атрибут name поля элемента ввода
