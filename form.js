c197e832b315f3736255c0570c47d2d1da64dfc0
sasha52205 <55660001+sasha52205@users.noreply.github.com>
2022-11-10 03:41:16 +0300

    Update form.js

--- form.js
+++ form.js
@@ -12,21 +12,7 @@ tg.MainButton.show()
 tg.MainButton.enable()
 
 
-document.addEventListener("DOMContentLoaded", function() { // событие загрузки страницы
 
-    // выбираем на странице все элементы типа textarea и input
-    document.querySelectorAll('input').forEach(function(e) {
-        // если данные значения уже записаны в sessionStorage, то вставляем их в поля формы
-        // путём этого мы как раз берём данные из памяти браузера, если страница была случайно перезагружена
-        if(e.value === '') e.value = window.sessionStorage.getItem(e.name, e.value);
-        // на событие ввода данных (включая вставку с помощью мыши) вешаем обработчик
-        e.addEventListener('input', function() {
-            // и записываем в sessionStorage данные, в качестве имени используя атрибут name поля элемента ввода
-            window.sessionStorage.setItem(e.name, e.value);
-        })
-    })
-
-}); 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
     fio = document.getElementById("name").value;
