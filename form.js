dd69d38844cfa620c333dded8372219efb1667dd
sasha52205 <55660001+sasha52205@users.noreply.github.com>
2022-11-10 03:38:22 +0300

    Update form.js

--- form.js
+++ form.js
@@ -12,25 +12,21 @@ tg.MainButton.show()
 tg.MainButton.enable()
 
 
-document.addEventListener('DOMContentLoaded', function(){
-    let formData = {};
-    alert(formData);
-    const form = document.getElementById('form');
-    alert(form);
-    const LS = localStorage;
+document.addEventListener("DOMContentLoaded", function() { // событие загрузки страницы
 
-    //получааем данные из input
-    form.addEventListener('input', function(event){
-        formData[event.target.name] = event.target.value;
-        alert(JSON.stringify(formData));
-        LS.setItem('formData', JSON.stringify(formData));
-     };
-      	//востановить
-     if (LS.getItem('formData')) {
-      	formData = JSON.parse(LS.getItem('formData'));
-        alert(formData);
- });
+    // выбираем на странице все элементы типа textarea и input
+    document.querySelectorAll('textarea, input').forEach(function(e) {
+        // если данные значения уже записаны в sessionStorage, то вставляем их в поля формы
+        // путём этого мы как раз берём данные из памяти браузера, если страница была случайно перезагружена
+        if(e.value === '') e.value = window.sessionStorage.getItem(e.name, e.value);
+        // на событие ввода данных (включая вставку с помощью мыши) вешаем обработчик
+        e.addEventListener('input', function() {
+            // и записываем в sessionStorage данные, в качестве имени используя атрибут name поля элемента ввода
+            window.sessionStorage.setItem(e.name, e.value);
+        })
+    })
 
+}); 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
     fio = document.getElementById("name").value;
